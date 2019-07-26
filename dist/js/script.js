f91379ad58beab57327b4b1ecb4d0afe7bf3f3e0
Thomas Claireau <thomas.claireau@gmail.com>
2019-07-26 23:25:16 +0200

    reactivation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const newGrid = new Grid();
     newGrid.createGrid();
-    // newGrid.setInaccessibleCase();
+    newGrid.setInaccessibleCase();
     const club = new Weapon('club', 10, newGrid.grid); // l'arme par défaut
     const dagger = new Weapon('dagger', 20, newGrid.grid);
     const axe = new Weapon('axe', 24, newGrid.grid);
     const hammer = new Weapon('hammer', 28, newGrid.grid);
     const sword = new Weapon('sword', 30, newGrid.grid);
-    // dagger.getWeapon();
-    // axe.getWeapon();
-    // hammer.getWeapon();
-    // sword.getWeapon();
+    dagger.getWeapon();
+    axe.getWeapon();
+    hammer.getWeapon();
+    sword.getWeapon();
     const player1 = new Player('Joueur 1', 100, club, newGrid.grid);
     const player2 = new Player('Joueur 2', 100, club, newGrid.grid);
     player1.placePlayer();
--- ts/script.ts
+++ ts/script.ts
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const newGrid = new Grid();
 	newGrid.createGrid();
-	// newGrid.setInaccessibleCase();
+	newGrid.setInaccessibleCase();
 
 	const club = new Weapon('club', 10, newGrid.grid); // l'arme par défaut
 	const dagger = new Weapon('dagger', 20, newGrid.grid);
@@ -9,10 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
 	const hammer = new Weapon('hammer', 28, newGrid.grid);
 	const sword = new Weapon('sword', 30, newGrid.grid);
 
-	// dagger.getWeapon();
-	// axe.getWeapon();
-	// hammer.getWeapon();
-	// sword.getWeapon();
+	dagger.getWeapon();
+	axe.getWeapon();
+	hammer.getWeapon();
+	sword.getWeapon();
 
 	const player1 = new Player('Joueur 1', 100, club, newGrid.grid);
 	const player2 = new Player('Joueur 2', 100, club, newGrid.grid);
