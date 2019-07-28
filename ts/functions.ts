0a33bb7d218f2895e084fd419cab445790e090bd
Thomas Claireau <thomas.claireau@gmail.com>
2019-07-28 18:55:33 +0200

    remove potAsyncLoop

--- ts/functions.ts
+++ ts/functions.ts
@@ -2,20 +2,3 @@ function pickANumber() {
 	// fonction pour créer un entier aléatoire
 	return Math.floor(Math.random() * 10);
 }
-
-function potAsyncLoop(i, resume) {
-	// evite de looper dans la methode createPlayer
-	let condition = false;
-	const max = 1000000;
-	if (i < max) {
-		if (i % 1000 === 0) {
-			setTimeout(function() {
-				potAsyncLoop(i + 1, resume);
-			}, 0);
-		} else {
-			potAsyncLoop(i + 1, resume);
-		}
-	} else {
-		resume;
-	}
-}
