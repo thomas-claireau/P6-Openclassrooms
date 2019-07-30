adda0674008d5157396b31f6c8b3183cdfcbee9e
Thomas Claireau <thomas.claireau@gmail.com>
2019-07-30 23:26:34 +0200

    custom front css -> en cours

--- dist/js/front.js
+++ dist/js/front.js
@@ -2,33 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnPlay = document.querySelector('.play button');
     const btnCustom = document.querySelector('.custom button');
     const option = document.querySelector('.container-custom .option-game');
-    const optionsGrid = option.querySelectorAll('.grid .select');
-    let listOptions = new Array();
+    const optionsGrid = option.querySelectorAll('.select');
+    // btnPlay.addEventListener('click', function() {
+    // 	// console.log('GOOOO !');
+    // });
+    // btnCustom.addEventListener('click', function() {
+    // 	listOptions.forEach((option) => {
+    // 		option.classList.remove('hide');
+    // 	});
+    // 	btnPlay.classList.add('remove');
+    // 	btnCustom.classList.add('remove');
+    // });
     optionsGrid.forEach((option) => {
-        listOptions.push(option);
-    });
-    console.log(listOptions);
-    btnPlay.addEventListener('click', function () {
-        console.log('GOOOO !');
-    });
-    btnCustom.addEventListener('click', function () {
-        const optionOne = listOptions[0];
-        optionOne.classList.remove('hide');
-        btnPlay.classList.add('hide');
-        btnCustom.classList.add('hide');
-        // init option
-        const slider = listOptions[0].querySelector('input');
-        const output = document.querySelector('label[for="nbCase"] .output');
-        output.innerHTML = slider.value; // Display the default slider value
-        // Update the current slider value (each time you drag the slider handle)
-        slider.oninput = function () {
-            output.textContent = slider.value;
+        const inputOption = option.querySelector('input');
+        const outputOption = option.querySelector('.output');
+        outputOption.innerHTML = inputOption.value;
+        inputOption.oninput = function () {
+            outputOption.textContent = inputOption.value;
         };
-    });
-    // next option
-    const nextButtons = document.querySelectorAll('.next');
-    nextButtons.forEach((nextButton) => {
-        const target = nextButton.dataset.id;
-        console.log(target);
     });
 });
